45cc78182534e961d5041f94694c04b4e2daddfa
Thomas Levine <occurrence@thomaslevine.com>
2013-04-11 00:44:22 -0400

    who signed up for both

---
+++ crossovers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+import os
+import json
+
+DIR = 'april-25-stats-downloads'
+
+def members(filename):
+    h = open(filename)
+    rsvps = json.load(h)
+    h.close()
+    return set([r['member']['name'] for r in rsvps['results'] if r['response'] == 'yes'])
+
+dk = members(os.path.join(DIR, 'datakind.json'))
+r  = members(os.path.join(DIR, 'nyhackr.json'))
+i = dk.intersection(r)
+
+print 'Attending DataKind: %d people' % len(dk)
+print '\n'.join(dk)
+print ''
+
+print 'Attending nyhackr: %d people' % len(r)
+print '\n'.join(r)
+print ''
+
+print 'Attending both: %d people' % len(i)
+print '\n'.join(i)
